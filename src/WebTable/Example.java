ef612e038f41738beda8780bb04aa7064c531ff3
USER NAME <EMAIL>
2019-07-13 22:09:23 +0530

    updated thread.sleep in webtable class

--- src/WebTable/Example.java
+++ src/WebTable/Example.java
@@ -16,7 +16,7 @@ public class Example {
 		
 		driver.findElement(By.id("onward_cal"));
 		
-		Thread.sleep(3000);
+		Thread.sleep(5000);
 		
 		driver.findElement(By.xpath("//div[@id='rb-calendar_return_cal']//tbody/tr[4]/td[4]")).click();
 		
